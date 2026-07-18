b7c85fa036552fbb79ceaaa1eabcdfb80f1ebc36
Shaqib Khan <shaqibkhan@Shaqibs-MacBook-Air.local>
2026-07-18 22:40:13 +0530

    day 2 completed

---
+++ day2.py
@@ -0,0 +1,46 @@
+        #   python fundamentals
+
+# variables
+
+name = "shaqib" 
+age = 20
+
+# data types
+
+name = "shaqib"
+print(name)
+print(type(name))
+
+age = 20
+print(age)
+print(type(age))
+
+is_completed = True # can also be false
+print(is_completed)
+print(type(is_completed))
+
+# type casting
+
+a = 34
+b = "34"
+
+print(b)
+print(type(b))
+
+# convert b to an integer
+c = int(b)
+print(c) 
+print(type(c))  
+
+# taking user input
+
+a = input("Enter a number")
+print(a)
+
+a = input("Enter a number: ")
+a = int(a)
+print(a+3)
+
+a = int(input("enter first number"))
+b = int(input("enter second number"))
+print(a + b)
